c881a16f498475f321ee635a13afe96dd498176e
LupeMM4652 <guadalupemezquita21@gmail.com>
2024-11-26 08:26:23 -0500

    Update pending_orders.py
    copied from pending orders sis app

--- pending_orders.py
+++ pending_orders.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -28,7 +28,7 @@ if my_dataframe:
                         )
             st.success('Someone clicked the button', icon = '👍')
         except:
-            st.write('Something went wrong.')
+            st.write('Something went wrong,' icon = '👎')
 
 else: 
-    st.success('There are no pending orders right now', icon = '👍')
+    st.success('There are no pending orders right now', icon = '✅')
